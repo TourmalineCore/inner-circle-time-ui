2456004676589856180fa61d54c5f2ecd3a50ac5
akovylyaeva <akovylyaeva@tourmalinecore.com>
2025-12-15 16:49:23 +0500

    refactor: remove /tracking from DEFAULT_PATH

--- src/pages/routes.tsx
+++ src/pages/routes.tsx
@@ -1,9 +1,9 @@
 import { TimeTrackerPage } from "./time-tracker/TimeTrackerPage"
 
-const DEFAULT_PATH = `/time/tracking`
+const DEFAULT_PATH = `/time`
 
 export const timeTrackerRoutes = {
-  path: DEFAULT_PATH,
+  path: `${DEFAULT_PATH}/tracking`,
   breadcrumb: `Time Tracker`,
   Component: TimeTrackerPage,
 }
