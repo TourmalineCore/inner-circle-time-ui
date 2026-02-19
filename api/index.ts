0ad922eb815b710f808e2093b4eccb435dcd6711
Dmitriy Myakotin <75628188+MDI74@users.noreply.github.com>
2026-02-19 11:51:53 +0500

    refactor: regenerate api types

--- api/index.ts
+++ api/index.ts
@@ -10,18 +10,6 @@
  * ---------------------------------------------------------------
  */
 
-export interface CreateUnwellEntryRequest {
-  /** @format date-time */
-  startTime: string;
-  /** @format date-time */
-  endTime: string;
-}
-
-export interface CreateUnwellResponse {
-  /** @format int64 */
-  newUnwellEntryId: number;
-}
-
 export interface CreateWorkEntryRequest {
   title: string;
   /** @format date-time */
@@ -39,11 +27,8 @@ export interface CreateWorkEntryResponse {
   newWorkEntryId: number;
 }
 
-export type EntryType = number;
-
 export interface GetWorkEntriesByPeriodResponse {
   workEntries: WorkEntryDto[];
-  unwellEntries: UnwellEntryDto[];
 }
 
 export interface ProjectDto {
@@ -54,23 +39,6 @@ export interface ProjectDto {
 
 export interface ProjectsResponse {
   projects: ProjectDto[];
-}
-
-export interface UnwellEntryDto {
-  /** @format int64 */
-  id: number;
-  /** @format date-time */
-  startTime: string;
-  /** @format date-time */
-  endTime: string;
-  type: EntryType;
-}
-
-export interface UpdateUnwellEntryRequest {
-  /** @format date-time */
-  startTime: string;
-  /** @format date-time */
-  endTime: string;
 }
 
 export interface UpdateWorkEntryRequest {
@@ -93,7 +61,6 @@ export interface WorkEntryDto {
   startTime: string;
   /** @format date-time */
   endTime: string;
-  type: EntryType;
   /** @format int64 */
   projectId: number;
   taskId: string;
@@ -334,27 +301,6 @@ export class Api<
      * No description
      *
      * @tags Tracking
-     * @name TrackingCreateUnwellEntry
-     * @summary Create an unwell entry
-     * @request POST:/api/time/tracking/unwell-entries
-     */
-    trackingCreateUnwellEntry: (
-      data: CreateUnwellEntryRequest,
-      params: RequestParams = {},
-    ) =>
-      this.request<CreateUnwellResponse, any>({
-        path: `/api/time/tracking/unwell-entries`,
-        method: "POST",
-        body: data,
-        type: ContentType.Json,
-        format: "json",
-        ...params,
-      }),
-
-    /**
-     * No description
-     *
-     * @tags Tracking
      * @name TrackingUpdateWorkEntry
      * @summary Update a work entry
      * @request POST:/api/time/tracking/work-entries/{workEntryId}
@@ -366,27 +312,6 @@ export class Api<
     ) =>
       this.request<void, any>({
         path: `/api/time/tracking/work-entries/${workEntryId}`,
-        method: "POST",
-        body: data,
-        type: ContentType.Json,
-        ...params,
-      }),
-
-    /**
-     * No description
-     *
-     * @tags Tracking
-     * @name TrackingUpdateUnwellEntry
-     * @summary Update an unwell entry
-     * @request POST:/api/time/tracking/unwell-entries/{unwellEntryId}
-     */
-    trackingUpdateUnwellEntry: (
-      unwellEntryId: number,
-      data: UpdateUnwellEntryRequest,
-      params: RequestParams = {},
-    ) =>
-      this.request<void, any>({
-        path: `/api/time/tracking/unwell-entries/${unwellEntryId}`,
         method: "POST",
         body: data,
         type: ContentType.Json,
