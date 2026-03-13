82ae737e1d6e5f227fce50b33b6d778d4738301c
Aleksandr Shinkarev <ashinkarev@tourmalinecore.com>
2026-03-13 16:22:46 +0500

    lint: fix code lint errors

--- src/common/api/api.ts
+++ src/common/api/api.ts
@@ -10,14 +10,14 @@ initApiInterceptors(apiClient.instance)
 
 // keep it after the auth interceptors in initApiInterceptors so that it runs first (interceptors are invoked in a reverse registration order)
 apiClient.instance.interceptors.request.use((config: any) => {
-    // this is needed to correctly combine baseUrl like http://localhost:30090/api/time and url like /api/tracking/entries
-    // into a correct local/prod env URL like http://localhost:30090/api/time/tracking/entries
-    // otherwise we get http://localhost:30090/api/time/api/tracking/entries which doesn't work
-    config.url = config.url.replace(`/api`, ``)
+  // this is needed to correctly combine baseUrl like http://localhost:30090/api/time and url like /api/tracking/entries
+  // into a correct local/prod env URL like http://localhost:30090/api/time/tracking/entries
+  // otherwise we get http://localhost:30090/api/time/api/tracking/entries which doesn't work
+  config.url = config.url.replace(`/api`, ``)
 
-    return config
-  }, 
-  null
+  return config
+}, 
+null,
 )
 
 export const api = apiClient.api
