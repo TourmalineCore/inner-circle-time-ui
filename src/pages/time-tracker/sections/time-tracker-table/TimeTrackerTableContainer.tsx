e27dd9a0077cd6dbb99597bb82995b6a918e99b4
Dmitriy Myakotin <75628188+MDI74@users.noreply.github.com>
2026-06-29 09:33:09 +0500

    refactor: #89: add entry mapper

--- src/pages/time-tracker/sections/time-tracker-table/TimeTrackerTableContainer.tsx
+++ src/pages/time-tracker/sections/time-tracker-table/TimeTrackerTableContainer.tsx
@@ -1,13 +1,13 @@
 import { observer } from "mobx-react-lite"
 import { useContext, useEffect, useState } from "react"
 import { TimeTrackerStateContext } from "./state/TimeTrackerTableStateContext"
-import moment from "moment"
 import { api } from "../../../../common/api/api"
 import { Views } from "react-big-calendar"
 import { useDeviceSize } from "../../../../common/hooks/useDeviceSize"
 import { eventBus, EventBusType } from "../../event-bus"
 import { TimeTrackerTableContent } from "./TimeTrackerTableContent"
 import { TrackedEntry } from "../../types"
+import { EntryMapper } from "./mapper/entryMapper"
 
 export const TimeTrackerTableContainer = observer(({
   isCopyMode,
@@ -88,7 +88,7 @@ export const TimeTrackerTableContainer = observer(({
 
     async function loadedEntries() {
       const {
-        data,
+        data: entries,
       } = await api.trackingGetEntriesByPeriod({
         startDate: viewStartDate as string,
         endDate: viewEndDate as string,
@@ -103,72 +103,12 @@ export const TimeTrackerTableContainer = observer(({
         endDate: viewEndDate!,
       })
 
-      const taskEntries = data
-        .taskEntries
-        .map((taskEntry) => ({
-          id: taskEntry.id,
-          taskId: taskEntry.taskId,
-          description: taskEntry.description,
-          project: projects.find((project) => project.id === taskEntry.projectId)!,
-          title: taskEntry.title,
-          type: taskEntry.type,
-          date: moment(taskEntry.startTime)
-            .toDate(),
-          start: moment(taskEntry.startTime)
-            .toDate(),
-          end: moment(taskEntry.endTime)
-            .toDate(),
-        }))
-
-      const unwellEntries = data
-        .unwellEntries
-        .map((unwellEntry) => ({
-          id: unwellEntry.id,
-          type: unwellEntry.type,
-          date: moment(unwellEntry.startTime)
-            .toDate(),
-          start: moment(unwellEntry.startTime)
-            .toDate(),
-          end: moment(unwellEntry.endTime)
-            .toDate(),
-        }))
-        
-      const awayWithMakeUpTimeEntries = data
-        .awayWithMakeUpTimeEntries
-        .map((awayWithMakeUpTimeEntry) => ({
-          id: awayWithMakeUpTimeEntry.id,
-          type: awayWithMakeUpTimeEntry.type,
-          description: awayWithMakeUpTimeEntry.description,
-          date: moment(awayWithMakeUpTimeEntry.startTime)
-            .toDate(),
-          start: moment(awayWithMakeUpTimeEntry.startTime)
-            .toDate(),
-          end: moment(awayWithMakeUpTimeEntry.endTime)
-            .toDate(),
-          makeUpTimeList: awayWithMakeUpTimeEntry.makeUpTimeList,
-        }))
-
-      const makeUpTimeEntries = data
-        .makeUpTimeEntries
-        .map((makeUpTimeEntry) => ({
-          relatedEntryId: makeUpTimeEntry.relatedEntryId,
-          type: makeUpTimeEntry.type,
-          date: moment(makeUpTimeEntry.startTime)
-            .toDate(),
-          start: moment(makeUpTimeEntry.startTime)
-            .toDate(),
-          end: moment(makeUpTimeEntry.endTime)
-            .toDate(),
-        }))
-
       timeTrackerState.initialize({
         loadedData: {
-          entries: [
-            ...taskEntries,
-            ...unwellEntries,
-            ...awayWithMakeUpTimeEntries,
-            ...makeUpTimeEntries,
-          ], 
+          entries: EntryMapper.toEntryList({
+            entries,
+            projects,
+          }), 
         },
       })
     }
---
+++ src/pages/time-tracker/sections/time-tracker-table/mapper/entryMapper.ts
@@ -0,0 +1,117 @@
+
+import { AwayWithMakeUpTimeEntryDto, GetEntriesByPeriodResponse, MakeUpTimeEntryWithRelatedEntryIdDto, ProjectDto, TaskEntryDto, UnwellEntryDto } from "@tourmalinecore/inner-circle-time-api-js-client"
+import moment from "moment"
+
+export class EntryMapper {
+  public static toEntryList({
+    entries,
+    projects,
+  }: {
+    entries: GetEntriesByPeriodResponse,
+    projects: ProjectDto[],
+  },
+  ) {
+    const {
+      taskEntries,
+      unwellEntries,
+      awayWithMakeUpTimeEntries,
+      makeUpTimeEntries, 
+    } = entries
+
+    return [
+      ...this.mapTaskEntries({
+        taskEntries,
+        projects,
+      }),
+      ...this.mapUnwellEntries({
+        unwellEntries,
+      }),
+      ...this.mapAwayWithMakeUpTimeEntries({
+        awayWithMakeUpTimeEntries,
+      }),
+      ...this.mapMakeUpTimeEntries({
+        makeUpTimeEntries,
+      }),
+    ]
+  }
+
+  private static mapTaskEntries({
+    taskEntries,
+    projects,
+  }: {
+    taskEntries: TaskEntryDto[],
+    projects: ProjectDto[],
+  } ) {
+    return taskEntries
+      .map((taskEntry) => ({
+        id: taskEntry.id,
+        taskId: taskEntry.taskId,
+        description: taskEntry.description,
+        project: projects.find((project) => project.id === taskEntry.projectId)!,
+        title: taskEntry.title,
+        type: taskEntry.type,
+        date: moment(taskEntry.startTime)
+          .toDate(),
+        start: moment(taskEntry.startTime)
+          .toDate(),
+        end: moment(taskEntry.endTime)
+          .toDate(),
+      }))
+  }
+
+  private static mapUnwellEntries({
+    unwellEntries,
+  }: {
+    unwellEntries: UnwellEntryDto[],
+  } ) {
+    return unwellEntries
+      .map((unwellEntry) => ({
+        id: unwellEntry.id,
+        type: unwellEntry.type,
+        date: moment(unwellEntry.startTime)
+          .toDate(),
+        start: moment(unwellEntry.startTime)
+          .toDate(),
+        end: moment(unwellEntry.endTime)
+          .toDate(),
+      }))
+  }
+
+  private static mapAwayWithMakeUpTimeEntries({
+    awayWithMakeUpTimeEntries,
+  }: {
+    awayWithMakeUpTimeEntries: AwayWithMakeUpTimeEntryDto[],
+  } ) {
+    return awayWithMakeUpTimeEntries
+      .map((awayWithMakeUpTimeEntry) => ({
+        id: awayWithMakeUpTimeEntry.id,
+        type: awayWithMakeUpTimeEntry.type,
+        description: awayWithMakeUpTimeEntry.description,
+        date: moment(awayWithMakeUpTimeEntry.startTime)
+          .toDate(),
+        start: moment(awayWithMakeUpTimeEntry.startTime)
+          .toDate(),
+        end: moment(awayWithMakeUpTimeEntry.endTime)
+          .toDate(),
+        makeUpTimeList: awayWithMakeUpTimeEntry.makeUpTimeList,
+      }))
+  }
+
+  private static mapMakeUpTimeEntries({
+    makeUpTimeEntries,
+  }: {
+    makeUpTimeEntries: MakeUpTimeEntryWithRelatedEntryIdDto[],
+  } ) {
+    return makeUpTimeEntries
+      .map((makeUpTimeEntry) => ({
+        relatedEntryId: makeUpTimeEntry.relatedEntryId,
+        type: makeUpTimeEntry.type,
+        date: moment(makeUpTimeEntry.startTime)
+          .toDate(),
+        start: moment(makeUpTimeEntry.startTime)
+          .toDate(),
+        end: moment(makeUpTimeEntry.endTime)
+          .toDate(),
+      }))
+  }
+}
