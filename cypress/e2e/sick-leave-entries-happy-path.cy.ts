204516b36ed196f2035c8c3fda1794bb26e3435c
Dmitriy Myakotin <75628188+MDI74@users.noreply.github.com>
2026-07-08 15:06:28 +0500

    test: #109: update sick-leave-entries-happy-path e2e test

--- cypress/e2e/sick-leave-entries-happy-path.cy.ts
+++ cypress/e2e/sick-leave-entries-happy-path.cy.ts
@@ -2,31 +2,24 @@ import { EntryType } from "../../src/common/constants/entryType"
 import { TrackingPageActions } from "../pagesActions/TrackingPageActions"
 
 describe(`Sick Leave Entry Happy Path`, () => {
-  const dateToDeleteFirstSickLeave = new Date(2021, 6, 5)
-  const dateToDeleteSecondSickLeave = new Date(2025, 6, 12)
+  const testDate = new Date(2021, 6, 5)
 
   beforeEach(`Set Date and Authorize and Cleanup`, () => {
     // set cypress default date
     // we use different years for different tests, which does not overlap
-    cy.clock(new Date(2021, 6, 5), [
+    cy.clock(testDate, [
       `Date`,
     ])
 
     cy.authByApi()
     cy.removeSickLeaveEntries({
-      date: dateToDeleteFirstSickLeave,
-    })
-    cy.removeSickLeaveEntries({
-      date: dateToDeleteSecondSickLeave,
+      date: testDate,
     })
   })
 
   afterEach(`Cleanup`, () => {
     cy.removeSickLeaveEntries({
-      date: dateToDeleteFirstSickLeave,
-    })
-    cy.removeSickLeaveEntries({
-      date: dateToDeleteSecondSickLeave,
+      date: testDate,
     })
   })
 
@@ -35,7 +28,7 @@ describe(`Sick Leave Entry Happy Path`, () => {
   AND user wants to track this sick leave in the time tracker
   WHEN user adds the sick leave period from Monday to Friday in the time tracker
   AND extends it until next Monday after the doctor's visit
-  THEN user should see sick leave for 8 days in the time tracker
+  THEN user should see 8-day sick leave in the time tracker
   `, () => {
     TrackingPageActions.visit()
 
@@ -70,17 +63,16 @@ describe(`Sick Leave Entry Happy Path`, () => {
       .should(`have.length`, 5)
 
     TrackingPageActions.getAllDayButton()
-
       .first()
       .click()
 
     cy.log(`Check that when opening the sick leave, the data in it is displayed correctly`)
 
     TrackingPageActions.getEntryModalStartDateDatepicker()
-      .should(`have.value`, `10.06`)
+      .should(`have.value`, `05.06`)
 
     TrackingPageActions.getEntryModalEndDateDatepicker()
-      .should(`have.value`, `12.06`)
+      .should(`have.value`, `09.06`)
 
     cy.log(`Update existing Sick Leave Entry`)
 
