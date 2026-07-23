cc141290fb3184b94f3c8b32e489cd8ffc6d8c05
Dmitriy Myakotin <75628188+MDI74@users.noreply.github.com>
2026-07-23 14:27:26 +0500

    refactor: remove redundant replace for '/api'

--- src/common/api/api.ts
+++ src/common/api/api.ts
@@ -8,18 +8,6 @@ const apiClient = new Api({
 
 initApiInterceptors(apiClient.instance)
 
-// keep it after the auth interceptors in initApiInterceptors so that it runs first (interceptors are invoked in a reverse registration order)
-apiClient.instance.interceptors.request.use((config: any) => {
-  // this is needed to correctly combine baseUrl like http://localhost:30090/api/time and url like /api/tracking/entries
-  // into a correct local/prod env URL like http://localhost:30090/api/time/tracking/entries
-  // otherwise we get http://localhost:30090/api/time/api/tracking/entries which doesn't work
-  config.url = config.url.replace(`/api`, ``)
-
-  return config
-}, 
-null,
-)
-
 // utility type that converts union to intersection.
 // used to flatten all API controller methods into a single object type.
 type UnionToIntersection<U> = (U extends any ? (k: U) => void : never) extends (k: infer I) => void ? I : never
