9907a07654613bf57a77653ced034df6287332b0
Dmitriy Myakotin <75628188+MDI74@users.noreply.github.com>
2026-07-23 13:48:40 +0500

    feat: add types for apiWithAllCombinedEndpoints

--- src/common/api/api.ts
+++ src/common/api/api.ts
@@ -20,11 +20,28 @@ apiClient.instance.interceptors.request.use((config: any) => {
 null,
 )
 
+// utility type that converts union to intersection.
+// used to flatten all API controller methods into a single object type.
+type UnionToIntersection<U> = (U extends any ? (k: U) => void : never) extends (k: infer I) => void ? I : never
+
+// get all methods from API controllers
+type ControllerValues = typeof apiClient[keyof typeof apiClient]
+
+// get final type with all API methods flattened
+type CombinedApi = UnionToIntersection<ControllerValues>
+
 // previously it contained only api object
 // now it contains an object per api controller, like tracking, reporting, internal
 // to keep things simple for ui we can combine all endpoints into an old single api facade object
 const objectsWithEdnpoints = Object.values(apiClient)
 
-const apiWithAllCombinedEndpoints = objectsWithEdnpoints.reduce((acc, curr) => ({ ...acc, ...curr }), {})
+const apiWithAllCombinedEndpoints = objectsWithEdnpoints
+  .reduce(
+    (acc, curr) => ({
+      ...acc,
+      ...curr, 
+    }), 
+    {},
+  ) as CombinedApi
 
 export const api = apiWithAllCombinedEndpoints
