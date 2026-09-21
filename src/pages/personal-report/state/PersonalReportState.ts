84bfb38c1b59359c4f14397bc21aa2da7a4f5034
Dmitriy Myakotin <75628188+MDI74@users.noreply.github.com>
2026-09-21 08:42:01 +0500

    refactor: #559: rename folder to consitency

--- src/pages/personal-report/state/PersonalReportState.ts
+++ src/pages/personal-report/state/PersonalReportState.ts
@@ -1,5 +1,5 @@
 import { makeAutoObservable } from 'mobx'
-import { formatToTwoDecimalPlaces } from '../../../common/utils/formatToTwoDecimalPlaces/formatToTwoDecimalPlaces'
+import { formatToTwoDecimalPlaces } from '../../../common/utils/format-to-two-decimal-places/formatToTwoDecimalPlaces'
 import { EmployeeDto, TrackedEntryDto } from '@tourmalinecore/inner-circle-time-api-js-client'
 import { PersonalReportData } from '../types'
 import moment from 'moment'
--- src/pages/time-tracker/sections/metrics/MetricsContent.tsx
+++ src/pages/time-tracker/sections/metrics/MetricsContent.tsx
@@ -3,8 +3,8 @@ import './Metrics.scss'
 import { observer } from "mobx-react-lite"
 import { useContext } from "react"
 import { MetricsStateContext } from "./state/MetricsStateContext"
-import { formatToTwoDecimalPlaces } from "../../../../common/utils/formatToTwoDecimalPlaces/formatToTwoDecimalPlaces"
-import { formatHoursAsHhMm } from '../../../../common/utils/formatHoursAsHhMm/formatHoursAsHhMm'
+import { formatToTwoDecimalPlaces } from "../../../../common/utils/format-to-two-decimal-places/formatToTwoDecimalPlaces"
+import { formatHoursAsHhMm } from '../../../../common/utils/format-hours-as-hh-mm/formatHoursAsHhMm'
 
 export const MetricsContent = observer(() => {
   const metricsState = useContext(MetricsStateContext)
